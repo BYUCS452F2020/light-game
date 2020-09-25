7e8aa2151bc2d545d82912fb50220dcd4c69fb0a
Aaron Egbert <aaronegbert@gmail.com>
2020-09-25 16:24:50 -0600

    added basic win-lose

--- client/src/app.ts
+++ client/src/app.ts
@@ -2,7 +2,13 @@ import * as Phaser from 'phaser'
 import { GameConfig } from './game_config'
 import { GameState } from './game_state'
 
-const game_config: Phaser.Types.Core.GameConfig = new GameConfig(800, 600, GameState).getGameConfig();
+// TODO: This is the goal, but different sized monitors have higher pixel densities than others.
+// Thus, rooms either
+// - need to be based on a large minimum fixed size
+// - need to zoom in for large screens
+// GameState.gameWidth = window.innerWidth;
+// GameState.gameHeight = window.innerHeight;
 GameState.gameWidth = 800;
 GameState.gameHeight = 600;
+const game_config: Phaser.Types.Core.GameConfig = new GameConfig(GameState.gameWidth, GameState.gameHeight, GameState).getGameConfig();
 const game: Phaser.Game = new Phaser.Game(game_config)
--- client/src/game_state.ts
+++ client/src/game_state.ts
@@ -30,6 +30,17 @@ export class GameState extends Phaser.Scene {
   light_polygon = new Phaser.Geom.Polygon()
   circle = new Phaser.Geom.Circle(this.circleX, this.circleY, 5);
 
+  // Player 2 position data
+  hiddenX: number = 500;
+  hiddenY: number = 500;
+
+  hiddenUP: Phaser.Input.Keyboard.Key;
+  hiddenDOWN: Phaser.Input.Keyboard.Key;
+  hiddenLEFT: Phaser.Input.Keyboard.Key;
+  hiddenRIGHT: Phaser.Input.Keyboard.Key;
+
+  hiddenCircle = new Phaser.Geom.Circle(this.hiddenX, this.hiddenY, 5);
+
   flashlightAngle = 90 * Math.PI/180;
   flashlightDirection = 90 * Math.PI/180;
   isFlashlight = true;
@@ -98,16 +109,26 @@ export class GameState extends Phaser.Scene {
   // Update number of edges
   this.numEdges = this.allEdges.length;
 
-  // Setup controls for user
-  this.keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-  this.keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
-  this.keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-  this.keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-  
-  this.keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
-  this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-  this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-  this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    // Setup controls for user
+    this.keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    this.keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+    this.keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    this.keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    
+    this.keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+    this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+
+    this.hiddenUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.I);
+    this.hiddenLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.J);
+    this.hiddenRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.L);
+    this.hiddenDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.K);
+
+    // Camera settings
+    this.cameras.main.setBounds(0, 0, GameState.gameWidth + 90, GameState.gameWidth);
+    this.cameras.main.setZoom(1);
+    this.cameras.main.centerOn(0, 0);
   }
 
   // Bounds an angle between -PI and PI (radians version of -180 -> 180 degrees)
@@ -122,7 +143,93 @@ export class GameState extends Phaser.Scene {
     return angle;
   }
 
-  handleKeyboardInput() {
+  handleMovement(currentX:number, currentY:number, nextPointX: number, nextPointY: number) {
+    const diffX: number = nextPointX - currentX;
+    const diffY: number = nextPointY - currentY;
+    const distanceAttemptingToTravel: number = Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2))
+    const rayAngle: number = Math.atan2(diffY, diffX); // Used for priority queue when added later
+    const raySlope = Math.tan(rayAngle);
+    const rayYIntercept: number = -(raySlope)*currentX + currentY
+
+    let collisionHappened = false;
+    let currentCollisionDistance = distanceAttemptingToTravel; // TODO: Change to max numrical value?
+    //console.log(`(x,y, nextPointX, nextPointY, travelDistance) => (${currentX},${currentY},${nextPointX},${nextPointY},${distanceAttemptingToTravel})`)
+    //console.log(`(diffx,diffy,travelDistance) => (${diffX},${diffY},${distanceAttemptingToTravel})`)
+    
+    let bestCollisionSoFarLineAngle: number = 0;
+  
+    // Checks for movement line collision with all polygon lines
+    for (let innerIndex = 0; innerIndex < this.numEdges; ++innerIndex) {
+      const currentEdge: Line = this.allEdges[innerIndex];
+      const edgeAngle: number = Math.atan2(currentEdge.y1 - currentEdge.y2, currentEdge.x1 - currentEdge.x2)
+      let collisionX: number;
+      let collisionY: number;
+
+      // Handles verticle polygon lines
+      // NOTE: Vertical `raySlope` is handled as a very large number, but not infinity
+      if (currentEdge.slope == Infinity || currentEdge.slope == -Infinity) {
+        collisionX = currentEdge.minX;
+        collisionY = raySlope*collisionX + rayYIntercept;
+      } else {
+        collisionX = (rayYIntercept - currentEdge.b) / (currentEdge.slope - raySlope);
+        collisionY = currentEdge.slope*collisionX + currentEdge.b;
+      }
+      
+      // Need a good enough buffer for floating point errors           
+      if (collisionX <= currentEdge.maxX + 0.00001 && collisionX >= currentEdge.minX - 0.00001 && 
+          collisionY <= currentEdge.maxY + 0.00001 && collisionY >= currentEdge.minY - 0.00001) {
+            // Also check if angle to circle is the same
+            const distanceToLightOrigin = Math.sqrt(Math.pow(collisionX - currentX, 2) + Math.pow(collisionY - currentY, 2))
+            const angleToLight = this.boundAngle(Math.atan2(collisionY - currentY, collisionX - currentX))
+
+            // if (currentEdge.x1 == 650 && currentEdge.y1 == 80) {
+            // console.log(`(angle) => (${distanceToLightOrigin},${angleToLight},${rayAngle},${rayAngle-2*Math.PI})`)
+            // }
+            
+            if (distanceToLightOrigin < currentCollisionDistance && //Check it is closer
+              ((angleToLight < rayAngle + 0.01 && angleToLight > rayAngle - 0.01) || // Three angle checks are required because of checking between -180 and 180 degrees sometimes happens on left movements
+              (angleToLight+2*Math.PI < rayAngle + 0.01 && angleToLight+2*Math.PI > rayAngle - 0.01) ||
+              (angleToLight-2*Math.PI < rayAngle + 0.01 && angleToLight-2*Math.PI > rayAngle - 0.01))) { //Check it is in the direction of the ray (including if ray angles jump from 0 to 360 or visa versa)
+                bestCollisionSoFarLineAngle = edgeAngle
+                currentCollisionDistance = distanceToLightOrigin
+                collisionHappened = true
+            }
+      }
+  
+      // TODO: Circumstance for when collisions are equal to line edge
+    }
+    
+    if (collisionHappened) {
+      // Smooth collisions, by only allowing movements in direction of the ray
+      const rawDistance = distanceAttemptingToTravel * Math.cos(rayAngle - bestCollisionSoFarLineAngle)
+      const xDiff = Math.cos(bestCollisionSoFarLineAngle) * rawDistance
+      const yDiff = Math.sin(bestCollisionSoFarLineAngle) * rawDistance
+
+      currentX += xDiff
+      currentY += yDiff
+    } else {
+      currentX = nextPointX
+      currentY = nextPointY
+    }
+
+    // Constrain circle position to inside room boundaries (small error boundary prevents parallelization with wall)
+    if (currentX > GameState.gameWidth - 0.0001) {
+      currentX = GameState.gameWidth - 0.0001;
+    }
+    if (currentX < 0) {
+      currentX = 0;
+    }
+    if (currentY > GameState.gameHeight - 0.0001) {
+      currentY = GameState.gameHeight - 0.0001;
+    }
+    if (currentY < 0) {
+      currentY = 0;
+    }
+
+    return [currentX,currentY];
+  }
+
+  handleLightPlayerKeyboardInput() {
     let nextPointX = this.circleX;
     let nextPointY = this.circleY;
 
@@ -165,98 +272,53 @@ export class GameState extends Phaser.Scene {
       return
     }
 
-    
-
-    const diffX: number = nextPointX - this.circleX;
-    const diffY: number = nextPointY - this.circleY;
-    const distanceAttemptingToTravel: number = Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2))
-    const rayAngle: number = Math.atan2(diffY, diffX); // Used for priority queue when added later
-    const raySlope = Math.tan(rayAngle);
-    const rayYIntercept: number = -(raySlope)*this.circleX + this.circleY
-
-    let collisionHappened = false;
-    let currentCollisionDistance = distanceAttemptingToTravel; // TODO: Change to max numrical value?
-    //console.log(`(x,y, nextPointX, nextPointY, travelDistance) => (${this.circleX},${this.circleY},${nextPointX},${nextPointY},${distanceAttemptingToTravel})`)
-    //console.log(`(diffx,diffy,travelDistance) => (${diffX},${diffY},${distanceAttemptingToTravel})`)
-    
-    let bestCollisionSoFarLineAngle: number = 0;
-  
-    // Checks for movement line collision with all polygon lines
-    for (let innerIndex = 0; innerIndex < this.numEdges; ++innerIndex) {
-      const currentEdge: Line = this.allEdges[innerIndex];
-      const edgeAngle: number = Math.atan2(currentEdge.y1 - currentEdge.y2, currentEdge.x1 - currentEdge.x2)
-      let collisionX: number;
-      let collisionY: number;
-
-      // Handles verticle polygon lines
-      // NOTE: Vertical `raySlope` is handled as a very large number, but not infinity
-      if (currentEdge.slope == Infinity || currentEdge.slope == -Infinity) {
-        collisionX = currentEdge.minX;
-        collisionY = raySlope*collisionX + rayYIntercept;
-      } else {
-        collisionX = (rayYIntercept - currentEdge.b) / (currentEdge.slope - raySlope);
-        collisionY = currentEdge.slope*collisionX + currentEdge.b;
-      }
-      
-      // Need a good enough buffer for floating point errors           
-      if (collisionX <= currentEdge.maxX + 0.00001 && collisionX >= currentEdge.minX - 0.00001 && 
-          collisionY <= currentEdge.maxY + 0.00001 && collisionY >= currentEdge.minY - 0.00001) {
-            // Also check if angle to circle is the same
-            const distanceToLightOrigin = Math.sqrt(Math.pow(collisionX - this.circleX, 2) + Math.pow(collisionY - this.circleY, 2))
-            const angleToLight = this.boundAngle(Math.atan2(collisionY - this.circleY, collisionX - this.circleX))
-
-            // if (currentEdge.x1 == 650 && currentEdge.y1 == 80) {
-            // console.log(`(angle) => (${distanceToLightOrigin},${angleToLight},${rayAngle},${rayAngle-2*Math.PI})`)
-            // }
-            
-            if (distanceToLightOrigin < currentCollisionDistance && //Check it is closer
-              ((angleToLight < rayAngle + 0.01 && angleToLight > rayAngle - 0.01) || // Three angle checks are required because of checking between -180 and 180 degrees sometimes happens on left movements
-              (angleToLight+2*Math.PI < rayAngle + 0.01 && angleToLight+2*Math.PI > rayAngle - 0.01) ||
-              (angleToLight-2*Math.PI < rayAngle + 0.01 && angleToLight-2*Math.PI > rayAngle - 0.01))) { //Check it is in the direction of the ray (including if ray angles jump from 0 to 360 or visa versa)
-                bestCollisionSoFarLineAngle = edgeAngle
-                currentCollisionDistance = distanceToLightOrigin
-                collisionHappened = true
-            }
-      }
-  
-      // TODO: Circumstance for when collisions are equal to line edge
-    }
-    
-    if (collisionHappened) {
-      // Smooth collisions, by only allowing movements in direction of the ray
-      const rawDistance = distanceAttemptingToTravel * Math.cos(rayAngle - bestCollisionSoFarLineAngle)
-      const xDiff = Math.cos(bestCollisionSoFarLineAngle) * rawDistance
-      const yDiff = Math.sin(bestCollisionSoFarLineAngle) * rawDistance
-
-      this.circleX += xDiff
-      this.circleY += yDiff
-    } else {
-      this.circleX = nextPointX
-      this.circleY = nextPointY
-    }
-
-    // Constrain circle position to inside room boundaries (small error boundary prevents parallelization with wall)
-    if (this.circleX > GameState.gameWidth - 0.0001) {
-      this.circleX = GameState.gameWidth - 0.0001;
-    }
-    if (this.circleX < 0) {
-      this.circleX = 0;
-    }
-    if (this.circleY > GameState.gameHeight - 0.0001) {
-      this.circleY = GameState.gameHeight - 0.0001;
-    }
-    if (this.circleY < 0) {
-      this.circleY = 0;
-    }
+    const returnValue = this.handleMovement(this.circleX, this.circleY, nextPointX, nextPointY)
+    this.circleX = returnValue[0]
+    this.circleY = returnValue[1]
   }
 
+  handleHiddenPlayerKeyboardInput() {
+    let nextPointX = this.hiddenX;
+    let nextPointY = this.hiddenY;
+
+    if (this.hiddenUP.isDown)
+    {
+      nextPointY -= 3
+    }
+    if (this.hiddenDOWN.isDown)
+    {
+      nextPointY += 3
+    }
+    if (this.hiddenLEFT.isDown)
+    {
+      nextPointX -= 3
+    }
+    if (this.hiddenRIGHT.isDown)
+    {
+      nextPointX += 3
+    }
+
+    if (nextPointX == this.hiddenX && nextPointY == this.hiddenY) {
+      return
+    }
+
+    const returnValue = this.handleMovement(this.hiddenX, this.hiddenY, nextPointX, nextPointY)
+    this.hiddenX = returnValue[0]
+    this.hiddenY = returnValue[1]
+  }
+
+  // Whether the hidden player was caught in the light the last frame
+  was_hidden_player_caught: boolean = false;
+  hidden_player_health: number = 100; // How many health points the hidden player has to survive against the light
+  
   // Is called on every frame
   update() {
 
     // Significantly enhances performance by removing previously rendered objects
     this.graphics.clear()
   
-    this.handleKeyboardInput()
+    this.handleLightPlayerKeyboardInput()
+    this.handleHiddenPlayerKeyboardInput()
 
     // Contrain flashlight bounds to unit circle angles
     if (this.flashlightAngle > 2*Math.PI) {
@@ -278,6 +340,8 @@ export class GameState extends Phaser.Scene {
   
   // Creating the circle 
   this.circle.setTo(this.circleX, this.circleY, 5);
+  // Hidden player circle 
+  this.hiddenCircle.setTo(this.hiddenX, this.hiddenY, 5);
   
   let rayAngleQueue = priorityQueue<{angle, x, y}>()
   
@@ -396,8 +460,45 @@ export class GameState extends Phaser.Scene {
   this.graphics.fillStyle(0xffff00)
   this.graphics.fillPoints(this.light_polygon.points, true);
   
+  // Draw both players
   this.graphics.fillStyle(0xff0000)
   this.graphics.fillCircleShape(this.circle);
+
+  this.graphics.fillStyle(0x0000ff)
+  this.graphics.fillCircleShape(this.hiddenCircle);
+
+  var cam = this.cameras.main;
+
+  // Check if hidden player is caught in the light
+
+  if (this.hidden_player_health > 0) {
+    if (this.light_polygon.contains(this.hiddenX, this.hiddenY)) {
+      if (!this.was_hidden_player_caught) {
+        this.cameras.main.flash(250, 255, 0, 0);
+        this.was_hidden_player_caught = true;
+      }
+
+      this.hidden_player_health -= 1;
+      
+      cam.pan(this.hiddenX, this.hiddenY, 2000, 'Elastic', true);
+      cam.zoomTo(2, 1000, 'Elastic', true);
+      
+      this.cameras.main.shake(100, 0.005);
+    } else {
+      if (this.was_hidden_player_caught) {
+        this.was_hidden_player_caught = false;
+      }
+      
+      cam.pan(0, 0, 2000, 'Elastic', true);
+      cam.zoomTo(1, 1000, 'Elastic', true);
+    }
+  } else {
+    // Game Is Over
+    cam.pan(0, 0, 2000, 'Elastic', true);
+    cam.zoomTo(1, 1000, 'Elastic', true);
+    let text = this.add.text(GameState.gameWidth/2 - 80, GameState.gameHeight/2 - 30, 'Light WINS!').setFontSize(34).setScrollFactor(0);
+    text.setShadow(1, 1, '#000000', 2);
+  }
   }
 }
 
